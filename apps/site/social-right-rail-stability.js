06a63cefc79e1110b44c6cf304432a32c51b0ad2
Rúbertt Ramires <54216027+Rubertt12@users.noreply.github.com>
2026-09-02 15:13:20 -0300

    Carrega estilo compacto do botão de editar avatar

--- apps/site/social-right-rail-stability.js
+++ apps/site/social-right-rail-stability.js
@@ -32,6 +32,7 @@
 
   loadScript('./social-shell-state-v2.js?v=20260902-1','ccSocialShellStateV2Js');
   loadScript('./social-profile-avatar-position-v1.js?v=20260902-1','ccSocialAvatarPositionV1Js');
+  loadCss('./social-avatar-button-compact-v1.css?v=20260902-1','ccSocialAvatarButtonCompactV1Css');
   loadCss('./social-notifications-v22.css?v=20260901-2','ccNotificationsV22Css');
   loadCss('./social-notifications-v23.css?v=20260902-1','ccNotificationsV23Css');
   loadScript('./social-notifications-v23.js?v=20260902-1','ccNotificationsV23Js');
